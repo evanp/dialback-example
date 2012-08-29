ce2d3a5e67c52f9d923274ac9c5780f22dd0103e
Evan Prodromou <evan@status.net>
2012-08-29 17:04:18 -0400

    Trim down to just the server

--- app.js
+++ app.js
@@ -1,14 +1,171 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
+  , Step = require("step")
+  , wf = require("webfinger")
   , http = require('http')
+  , https = require('https')
+  , url = require('url')
+  , querystring = require('querystring')
   , path = require('path');
 
 var app = express();
+
+var discoverHostEndpoint = function(host, callback) {
+
+    Step(
+        function() {
+            wf.hostmeta(host, this);
+        },
+        function(err, jrd) {
+            var dialbacks;
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            if (!jrd.hasOwnProperty("links")) {
+                callback(new Error("No links in host-meta for " + host), null);
+                return;
+            }
+            dialbacks = jrd.links.filter(function(link) {
+                return (link.hasOwnProperty("rel") && link.rel == "dialback");
+            });
+            if (dialbacks.length === 0) {
+                callback(new Error("No dialback links in host-meta for " + host), null);
+                return;
+            }
+            callback(null, dialbacks[0]);
+        }
+    );
+
+};
+
+var discoverWebfingerEndpoint = function(address, callback) {
+
+    Step(
+        function() {
+            wf.hostmeta(address, this);
+        },
+        function(err, jrd) {
+            var dialbacks;
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            if (!jrd.hasOwnProperty("links")) {
+                callback(new Error("No links in lrdd for " + address), null);
+                return;
+            }
+            dialbacks = jrd.links.filter(function(link) {
+                return (link.hasOwnProperty("rel") && link.rel == "dialback");
+            });
+            if (dialbacks.length === 0) {
+                callback(new Error("No dialback links in lrdd for " + address), null);
+                return;
+            }
+            callback(null, dialbacks[0]);
+        }
+    );
+};
+
+var discoverEndpoint = function(fields, callback) {
+    if (fields.hasOwnProperty("host")) {
+        discoverHostEndpoint(fields.host, callback);
+    } else if (fields.hasOwnProperty("webfinger")) {
+        discoverWebfingerEndpoint(fields.webfinger, callback);
+    }
+};
+
+var postToEndpoint = function(endpoint, fields, callback) {
+    var options = url.parse(endpoint);
+    options.method = "POST";
+
+};
+
+var dialback = function(req, res, next) {
+
+    var auth,
+        fields,
+        unauthorized = function() {
+            res.status(401);
+            res.set({
+                "WWW-Authentication": "Dialback",
+                "Content-Type": "text/plain"
+            });
+            res.send("Unauthorized");
+        },
+        parseFields = function(str) {
+            var fstr = str.substr(9); // everything after "Dialback "
+            var pairs = fstr.split(/\s+/); // XXX: won't handle blanks inside values well
+            var fields = {};
+            pairs.forEach(function(pair) {
+                var kv = pair.split("="),
+                    key = kv[0],
+                    value = kv[1].replace(/^"|"$/g, "");
+                fields[key] = value;
+            });
+            return fields;
+        };
+
+    if (!req.headers.hasOwnProperty("authorization")) {
+        unauthorized();
+        return;
+    }
+
+    auth = req.headers.authorization;
+
+    if (auth.substr(0, 9) != "Dialback ") {
+        unauthorized();
+        return;
+    }
+
+    fields = parseFields(auth);
+
+    // must have a nonce
+
+    if (!fields.hasOwnProperty("nonce")) {
+        unauthorized();
+        return;
+    }
+
+    // must have a webfinger or host field
+
+    if (!fields.hasOwnProperty("host") && !fields.hasOwnProperty("webfinger")) {
+        unauthorized();
+        return;
+    }
+
+    fields.url = req.originalUrl;
+    
+    if (req.headers.hasOwnProperty("date")) {
+        fields.date = req.headers.date;
+    }
+
+    Step(
+        function() {
+            discoverEndpoint(fields, this);
+        },
+        function(err, endpoint) {
+            if (err) throw err;
+            postToEndpoint(endpoint, fields, this);
+        },
+        function(err, res) {
+            if (err) {
+                next(err);
+            } else if (res.statusCode !== 200) {
+                unauthorized();
+            } else if (fields.hasOwnProperty("host")) {
+                req.host = fields.host;
+                next();
+            } else if (fields.hasOwnProperty("webfinger")) {
+                req.webfinger = fields.webfinger;
+                next();
+            }
+        }
+    );
+};
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -26,7 +183,23 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+/**
+ * Add two numbers and return the result
+ */
+
+var add = function(req, res, next) {
+    var augend, addend;
+
+    try {
+        augend = parseInt(req.body.augend, 10);
+        addend = parseInt(req.body.addend, 10);
+        res.send(augend + addend);
+    } catch (err) {
+        next(err);
+    }
+};
+
+app.post('/add', dialback, add);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
