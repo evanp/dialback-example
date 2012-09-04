6bbb7d77a7d7a9c0ddf59252374c1e6f216139c7
Evan Prodromou <evan@status.net>
2012-09-04 12:23:24 -0400

    more work, still not working

--- client.js
+++ client.js
@@ -9,14 +9,37 @@ var express = require('express')
   , https = require('https')
   , url = require('url')
   , querystring = require('querystring')
-  , path = require('path');
+  , path = require('path')
+  , config = require('./config')
+  , crypto = require("crypto");
+
+var randomString = function(bytes, callback) {
+
+    crypto.randomBytes(bytes, function(err, buf) {
+        var str;
+
+        if (err) {
+            callback(err, null);
+        } else {
+            str = buf.toString("base64");
+
+            // XXX: optimize me
+
+            // XXX: optimize me
+
+            str = str.replace(/\+/g, "-");
+            str = str.replace(/\//g, "_");
+            str = str.replace(/=/g, "");
+
+            callback(null, str);
+        }
+    });
+};
 
 var app = express();
 
-var port = process.env.PORT || 3001;
-
 app.configure(function(){
-  app.set('port', port);
+  app.set('port', 80);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -30,11 +53,11 @@ app.get("/.well-known/host-meta.json", function(req, res) {
             {
                 rel: "lrdd",
                 type: "application/json",
-                template: "http://localhost:"+port+"/lrdd.json?uri={uri}"
+                template: "http://"+config.addclient+"/lrdd.json?uri={uri}"
             },
             {
                 rel: "dialback",
-                href: "http://localhost:"+port+"/dialback"
+                href: "http://"+config.addclient+"/dialback"
             }
         ]
     });
@@ -50,7 +73,7 @@ app.get("/lrdd.json", function(req, res) {
         links: [
             {
                 rel: "dialback",
-                href: "http://localhost:"+port+"/dialback"
+                href: "http://"+config.addclient+"/dialback"
             }
         ]
     });
@@ -60,17 +83,17 @@ var requests = {
 };
 
 app.post('/dialback', function(req, res, next) {
-
+    
 });
 
-app.get("/", function(req, res, next) {
+function showForm(req, res) {
     res.send("<html>" + 
              "<head>" +
              "<title>Add two numbers</title>" +
              "</head>" +
              "<body>" +
              "<h1>Add two numbers</h1>" +
-             "<form>" +
+             "<form action='/' method='post'>" +
              "<input type='text' name='augend' size='3'/> + " +
              "<input type='text' name='addend' size='3'/> = " +
              "<input type='text' name='result' size='3'/>" +
@@ -78,9 +101,51 @@ app.get("/", function(req, res, next) {
              "</form>" +
              "</body>" +
              "</html>");
+};
+
+app.get("/", function(req, res, next) {
+    showForm(res);
 });
 
+var calls = {};
+
+var dialbackCall = function(url, params, callback) {
+    var now = Date.now();
+
+    Step(
+        function() {
+            randomString(4, this);
+        },
+        function(err, str) {
+            if (err) throw err;
+            if (!calls.hasOwnProperty(url)) {
+                calls[url] = {};
+            }
+            if (!calls[url].hasOwnProperty(now)) {
+                calls[url][now] = [];
+            }
+            calls[url][now].push(str);
+        }
+    );
+};
+
 app.post("/", function(req, res, next) {
+    var augend = parseInt(req.body.augend, 10),
+        addend = parseInt(req.body.addend, 10),
+        url = "http://" + config.addserver + "/add";
+        
+    Step(
+        function() {
+            dialbackCall(url, {augend: augend, addend: addend}, this);
+        },
+        function(err, body, res) {
+            if (err) {
+                next(err);
+            } else {
+                showForm(res, augend, addend, body.result);
+            }
+        }
+    );
 });
 
 app.configure('development', function(){
